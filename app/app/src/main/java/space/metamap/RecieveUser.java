b907fb980ae986ca39c4dd24f52a778f7f53bbd5
seany067 <sean@escreet.co.uk>
2020-02-01 15:22:35 +0000

    Tried to do stuff

---
+++ app/app/src/main/java/space/metamap/RecieveUser.java
@@ -0,0 +1,52 @@
+package space.metamap;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import android.content.Context;
+
+import org.json.JSONObject;
+import org.json.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecieveUser {
+
+    public RecieveUser(Context context, final String username, final String password) {
+        String url = "https://metamapp.herokuapp.com/login";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response.substring(0, 500));
+                try {
+                    JSONObject json = new JSONObject(response);
+                }
+                catch(JSONException e) {
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString());
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> data = new HashMap<>();
+                data.put("username", username);
+                data.put("password", password);
+                return data;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+}
