33f0540b9b3fe5073277d8419e624337f90f0d2a
seany067 <sean@escreet.co.uk>
2020-02-02 02:20:12 +0000

    B I G  C O M M I E

--- app/app/src/main/java/space/metamap/RecievePosts.java
+++ app/app/src/main/java/space/metamap/RecievePosts.java
@@ -1,30 +1,45 @@
 package space.metamap;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BaseHttpStack;
+import com.android.volley.toolbox.HttpClientStack;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.radar.sdk.model.Coordinate;
 import space.metamap.util.Request;
+
+import static android.content.Context.MODE_PRIVATE;
 
 class RecievePosts {
 
     public static void receivePost(final Context context, final double latitude, final double longitude, final PostList postList) {
         String url = "https://metamapp.herokuapp.com/post";
+        //String url = "http://10.2.16.126:5000/post";
+        //String url = "http://192.168.128.63:5000/post";
+
+
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
 
         try {
-            Request jsonRequest = new Request(Request.Method.POST, url, new JSONObject(String.format("{\"operation\": \"get\",\"coordinates\": \"[%f, %f]\"}", latitude, longitude)), new Response.Listener<JSONObject>() {
+            System.out.println(String.format("{\"operation\": \"get\",\"coordinates\": \"[%f, %f]\"}", latitude, longitude));
+            Request jsonRequest = new Request(Request.Method.POST, url, new JSONObject(String.format("{\"operation\": \"get\",\"coordinates\": [%f, %f]}", latitude, longitude)), new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
+                    System.out.println("THIS IS A THING" + response);
                     try {
                         JSONArray data = response.getJSONArray("posts");
                         for (int i=0; i<data.length(); i++) {
@@ -42,7 +57,16 @@ class RecievePosts {
                 public void onErrorResponse(VolleyError error) {
                     System.err.println(error.toString());
                 }
-            });
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    SharedPreferences preferences = context.getSharedPreferences("metamapp", MODE_PRIVATE);
+
+                    HashMap<String, String> headers = new HashMap<String, String>();
+                    headers.put("Cookie", "flask_sess=" + preferences.getString("session", "").split(";")[0]);
+                    return headers;
+                }
+            };
             requestQueue.add(jsonRequest);
         }
         catch(Exception e) {
--- app/app/src/main/java/space/metamap/ui/feed/FeedFragment.java
+++ app/app/src/main/java/space/metamap/ui/feed/FeedFragment.java
@@ -1,6 +1,12 @@
 package space.metamap.ui.feed;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +15,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import space.metamap.MainActivity;
 import space.metamap.Post;
 import space.metamap.PostList;
 import space.metamap.R;
 import space.metamap.postelements.PostElement;
+import space.metamap.util.LocationInterface;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class FeedFragment extends Fragment {
 
@@ -23,22 +41,62 @@ public class FeedFragment extends Fragment {
 	private ArrayAdapter<PostElement> adapter;
 	private TextView coords;
 
-	public View onCreateView(@NonNull LayoutInflater inflater,
-							 ViewGroup container, Bundle savedInstanceState) {
-		View root = inflater.inflate(R.layout.fragment_feed, container, false);
+	public void setTextField(Location l) {
+		coords.setText(String.format("%f, %f", l.getLongitude(), l.getLatitude()));
+	}
 
-		PostList posts = new PostList();
 
-		coords = root.findViewById(R.id.coords);
-
-		feedList = root.findViewById(R.id.feedList);
-		adapter = new ArrayAdapter<>(container.getContext(), R.layout.post);
+	public void updatePosts(PostList posts) {
+		adapter = new ArrayAdapter<>(getContext(), R.layout.post);
 
 		for(Post post : posts.getList()) {
 			adapter.add(new PostElement(post));
 		}
 
 		feedList.setAdapter(adapter);
+	}
+
+	public View onCreateView(@NonNull LayoutInflater inflater,
+							 ViewGroup container, final Bundle savedInstanceState) {
+		final View root = inflater.inflate(R.layout.fragment_feed, container, false);
+		MainActivity mainActivity = (MainActivity) getActivity();
+
+		final PostList posts = mainActivity.getPosts();
+		feedList = root.findViewById(R.id.feedList);
+
+
+		FusedLocationProviderClient locationProvider = LocationServices.getFusedLocationProviderClient(getContext());
+		final Location location = new Location(LocationManager.GPS_PROVIDER);
+
+		ActivityCompat.requestPermissions(getActivity(), new String[] { ACCESS_FINE_LOCATION }, 1);
+
+		if(ActivityCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			return null;
+		}
+		locationProvider.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+			@Override
+			public void onSuccess(Location loc) {
+				coords = root.findViewById(R.id.coords);
+				setTextField(loc);
+				location.set(loc);
+				posts.getRetrieveList(getContext(), loc.getLatitude(), loc.getLongitude());
+			}
+		});
+
+		updatePosts(posts);
+
+		locationProvider.requestLocationUpdates(LocationRequest.create(), new LocationCallback() {
+			@Override
+			public void onLocationResult(LocationResult locationResult) {
+				if (locationResult == null) {
+					return;
+				}
+				for (Location loc : locationResult.getLocations()) {
+					location.set(loc);
+					setTextField(loc);
+				}
+			};
+		}, Looper.getMainLooper());
 
 		return root;
 	}
